1014bc0c71e7876a9648d8b459f8be4999f69d05
shahirdevp <shahirkm@5ine.in>
2019-09-17 17:04:39 +0530

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var toll = document.querySelectorAll('.tollfree');
+    var instances = M.Tooltip.init(toll);
+});
