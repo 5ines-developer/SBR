34c8ed0cc692d6755fbf0cae8c7de48cc1ce3482
Rishabh5ine <rishabhm@5ine.in>
2020-03-03 19:49:46 +0530

    testimonial-error-slider

--- application/views/site/testimonial.php
+++ application/views/site/testimonial.php
@@ -258,20 +258,20 @@
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1,
+                    slidesToScroll: 2,
                     infinite: true,
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 992,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: false
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 767,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
