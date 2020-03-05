a8b3b44cb8cd778ca3cfca693c831afd1c5e8c97
Rishabh5ine <rishabhm@5ine.in>
2020-03-05 18:23:50 +0530

    landing-page

---
+++ application/views/includes/landing-footer.php
@@ -0,0 +1,32 @@
+<section class="sec-footer bg-col">
+    <div class="container-fluide">
+        <div class="row m0">
+            <div class="col l8 m8 s12">
+                <div class="landing-footer">
+                    <h2>ShaadiBaraati</h2>
+                    <a href="<?php echo base_url('about-us') ?>">
+                        <p>About Us</p>
+                    </a>
+                    <a href="<?php echo base_url('privacy-policy') ?>">
+                        <p>Privacy Policy</p>
+                    </a>
+                    <a href="<?php echo base_url('terms-conditions') ?>">
+                        <p>Terms & Condition</p>
+                    </a>
+                </div>
+                <div class="copy-land">
+                    <p>© Shaadibaraati.com 2020. All right reserved by Baraati Media &amp; Entertainment Pvt Ltd </p>
+                </div>
+            </div>
+            <div class="col l4 m4 s12">
+                <div class="call-to-action">
+                    <h5>
+                        You Can directly call us!
+                    </h5>
+                    <p>Give Us call and discuss your requirements.</p>
+                    <a href="#">Call us on 1800 4199 456</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- application/views/site/landing-page.php
+++ application/views/site/landing-page.php
@@ -7,19 +7,195 @@
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/slick/slick.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/slimselect.min.css"> 
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/slimselect.min.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
-     <?php $this->load->view('includes/favicon.php');  ?>
-    
-
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/landing.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    <?php $this->load->view('includes/favicon.php');  ?>
 </head>
 
 <body>
- <!-- header -->
- <?php $this->load->view('includes/header.php'); ?>
+    <!-- header -->
+    <?php $this->load->view('includes/landing-header.php'); ?>
     <!-- end header -->
 
+    <!-- banner -->
+    <section class="landing-banner">
+        <div class="container-fluide">
+            <div class="row">
+                <div class="col l5 m5 s12">
+                    <div class="title-landing">
+                        <h5>Find the Perfect WEDDING PLANER in Bangalore</h5>
+                        <p>Customize and send free Online Invitation for your Mehendi, Engagement, Wedding And Reception Events using our wide selection of templates. </p>
+                    </div>
+                    <div class="title-answer">
+                        <h3>Answer 3 Questions & see best suitable planners</h3>
+                    </div>
+                </div>
+                <div class="col l7 m7 s12">
+                    <div class="form-slider">
+                        <div class="swiper-container">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide"><img src="
+                                    https://swiperjs.com/demos/images/nature-5.jpg" alt=""></div>
+                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-2.jpg)"></div>
+                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-3.jpg)"></div>
+                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-4.jpg)"></div>
+                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-5.jpg)"></div>
+                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-6.jpg)"></div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sec">
+        <div class="container-fluide">
+            <div class="row">
+                <div class="col l12 m12 s12">
+                    <div class="vender-detail">
+                        <h4>About Us</h4>
+                        <img src="<?php echo base_url() ?>assets/img/saprator.png" class="img-responsive" alt="">
+                        <p>Shaadi Baraati was established to bring a real, high quality Online Wedding Market to Pan India Level. Our passion for excellence is what inspired us in the beginning and it continues to drive us today. We pride ourselves in the
+                            superior experience of shopping in our Wedding Store and in the long-term relationships we’ve built with our customers. People come back to Shaadibaraati.com because they know they’ll find what they’re looking for on our shelves
+                            - and if they don’t, we’ll help them find it. Getting a customer's satisfaction is very hard. But all the customers in Shaadi Bharaati gives a most satisfied feedback in their wedding. We provide a service based on the customers
+                            need is a best thing we follow.</p>
+                        <h2>" Because it’s your wedding, it should be unique! "</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <section class="sec bg-col">
+        <div class="container-fluide">
+            <div class="row">
+                <div class="col l12 m12 s12">
+                    <div class="vender-detail">
+                        <h4>Wedding Vogue</h4>
+                        <img src="<?php echo base_url() ?>assets/img/saprator.png" class="img-responsive" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col l4 m4 s12">
+                    <div class="wed-dd">
+                        <img src="assets/img/vender/wp3.jpg" class="img-responsive" alt="">
+                        <div class="wed-pare">
+                            <h3>Different Types of Bridal Makeup Looks to Bring</h3>
+                            <p>Shaadi Baraati was established to bring a real, high quality Online Wedding Market to Pan India Level.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col l4 m4 s12">
+                    <div class="wed-dd">
+                        <img src="assets/img/vender/wp3.jpg" class="img-responsive" alt="">
+                        <div class="wed-pare">
+                            <h3>Different Types of Bridal Makeup Looks to Bring</h3>
+                            <p>Shaadi Baraati was established to bring a real, high quality Online Wedding Market to Pan India Level.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col l4 m4 s12">
+                    <div class="wed-dd">
+                        <img src="assets/img/vender/wp3.jpg" class="img-responsive" alt="">
+                        <div class="wed-pare">
+                            <h3>Different Types of Bridal Makeup Looks to Bring</h3>
+                            <p>Shaadi Baraati was established to bring a real, high quality Online Wedding Market to Pan India Level.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec">
+        <div class="container-fluide">
+            <div class="row">
+                <div class="col l12 m12 s12">
+                    <div class="center-align get-land">
+                        <h4>Get In Touch</h4>
+                        <p>Subscribe to Shaadi Baraati and Get access to our latest Blogs, Wedding Ideas and get the best quotes from trusted Wedding Vendors. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="contatct-item">
+                        <div class="col s6 m4">
+                            <p> <i class="material-icons">mail</i> </p>
+                            <p>Mail</p>
+                            <p><a href="mailto:support@shaadibaraati.com">support@shaadibaraati.com</a></p>
+                        </div>
+                        <div class="col s6 m4">
+                            <p> <i class="material-icons">call</i> </p>
+                            <p>Toll Free</p>
+                            <p><a href="tel:18004199456">1800 4199 456</a></p>
+                        </div>
+                        <div class="col s12 m4 smslayout">
+                            <p> <i class="material-icons">sms</i> </p>
+                            <p>For Details TYPE <b>"BARAATI"</b> space your name and send it to 567678080</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php $this->load->view('includes/landing-footer.php'); ?>
+
+    <!-- script -->
+    <script src="<?php echo base_url()?>assets/js/jquery-3.4.1.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/materialize.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/vue.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url()?>assets/css/slick/slick.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/axios.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/slimselect.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/script.js"></script>
+    <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+
+    <script>
+        var swiper = new Swiper('.swiper-container', {
+            effect: 'flip',
+            grabCursor: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    </script>
 </body>
 
 </html>
