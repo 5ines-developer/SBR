2f213711d74513208086f07200c9b7cfcca86d3a
Rishabh5ine <rishabhm@5ine.in>
2020-03-06 17:41:59 +0530

    landing-page-form

--- application/views/site/landing-page.php
+++ application/views/site/landing-page.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/slimselect.min.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/landing.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/swiper.css">
     <?php $this->load->view('includes/favicon.php');  ?>
 </head>
 
@@ -32,26 +32,86 @@
                         <h3>Answer 3 Questions & see best suitable planners</h3>
                     </div>
                 </div>
-                <div class="col l7 m7 s12">
+                <div class="col l6 m6 s12">
                     <div class="form-slider">
-                        <div class="swiper-container">
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide"><img src="
-                                    https://swiperjs.com/demos/images/nature-5.jpg" alt=""></div>
-                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-2.jpg)"></div>
-                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-3.jpg)"></div>
-                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-4.jpg)"></div>
-                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-5.jpg)"></div>
-                                <div class="swiper-slide" style="background-image:url(https://swiperjs.com/demos/images/nature-6.jpg)"></div>
+                        <div class="filter-boxs">
+                            <div class="slides">
+                                <div id="slide-1" style="opacity: 1; width: 480px;" class="active">
+                                    <div class="slide-header"><span class="point-no">1</span>
+                                        <h2>What type of planning is required?</h2>
+                                    </div>
+                                    <span class="input-options next-one " data-option="1,2,3" data-field="#planning_options"><div class="filter-p1 type-label border-bottom vndr-ct-filtrflw-0-nxt-web"><h6>Full Wedding Planning</h6><span class="sub-text" style="pointer-events: none;">Booking all services + coordination + execution, etc.</span>
+                                    <i class="material-icons">chevron_right</i>
+                                </div>
+                                </span>
+                                <span class="input-options next-one" data-option="1,2,4" data-field="#planning_options"><div class="filter-p1 type-label border-bottom vndr-ct-filtrflw-0-nxt-web"><h6>Partial Planning</h6><span class="sub-text" style="pointer-events: none;">Booking 2-3 services OR just coordination OR just execution</span>
+                                <i class="material-icons">chevron_right</i></div>
+                            </span>
+                        </div>
+                        <div id="slide-2" class="" style="opacity: 1; width: 480px;">
+                            <div class="slide-header">
+                                <span class="point-no">2</span>
+                                <h2>Your total wedding budget?</h2>
                             </div>
-                            <!-- Add Pagination -->
-                            <div class="swiper-pagination"></div>
+                            <span class="input-options next-two" data-option="1" data-field="#budget_options"><div class="filter-p1 budget-label two-slide-options border-bottom vndr-ct-filtrflw-1-nxt-web">Less than ₹ 15L <i class="material-icons arrow-p">chevron_right</i></div></span>
+                            <span class="input-options next-two" data-option="2" data-field="#budget_options"><div class="filter-p1 budget-label two-slide-options border-bottom vndr-ct-filtrflw-1-nxt-web">₹ 15L - 30L <i class="material-icons arrow-p">chevron_right</i></div></span>
+                            <span class="input-options next-two" data-option="3" data-field="#budget_options"><div class="filter-p1 budget-label two-slide-options border-bottom vndr-ct-filtrflw-1-nxt-web">₹ 30L - 60L <i class="material-icons arrow-p">chevron_right</i></div></span>
+                            <span class="input-options next-two" data-option="4" data-field="#budget_options"><div class="filter-p1 budget-label two-slide-options vndr-ct-filtrflw-1-nxt-web">More than ₹ 60L <i class="material-icons arrow-p">chevron_right</i></div></span>
+                            <div class="slide-footer">
+                                <span class="input-options back-one" data-option="" data-field="#budget_options"><button class="action-back vndr-ct-filtrflw-1-bck-web"> <i class="material-icons arrow-btn">chevron_left</i> Back </button></span>
+                            </div>
+                        </div>
+                        <div id="slide-3">
+                            <div class="slide-header">
+                                <span class="point-no">3</span>
+                                <h2>Choose your event date</h2>
+                            </div>
+                            <div class="third-slide-container">
+                                <div class="calender-container cc-date">
+                                    <input type="text" id="datep" placeholder="Select Your Event Date">
+                                    <span class="next-three"><div class="budget-label no-border third-slide-next-but pad-left but-slide-3-next input-options vndr-ct-filtrflw-2-nxt-web" data-option="" data-field="#event_dates">Dates not decided yet <i class="material-icons checker-btn">chevron_right</i></div></span>
+                                </div>
+                                <div class="slide-footer">
+                                    <span class="back-two"><button class="action-back vndr-ct-filtrflw-2-bck-web"><i class="material-icons arrow-btn">chevron_left</i> Back </button></span>
+                                    <span class="next-three"><button class="action-pink third-slide-next-but show-on-select vndr-ct-filtrflw-2-nxt-web">Submit</button></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="slide-4" style="opacity: 0; width: 0px;" class="">
+                            <div class="form-container">
+                                <span class="result-found">
+                            <span style="font-weight:400;">We have found</span>
+                                <br>Lots of WEDDING PLANNERS
+                                <span class="wow flip center" data-wow-iteration="100" data-wow-duration="1s">🎊</span>
+                                <br>matching your requirements!
+                                </span>
+                                <p class="hide-on-phone">Signup to view them &amp; get quotes</p>
+                                <form class="jqueryform2" novalidate="novalidate">
+                                    <div class="field">
+                                        <input type="text" name="fullname" onchange="document.getElementById('fullname').value = $(this).val();" required="">
+                                        <label for="fullname">Name</label>
+                                    </div>
+                                    <div class="field">
+                                        <input type="email" name="email" onchange="document.getElementById('email').value = $(this).val();" required="">
+                                        <label for="email">Email</label>
+                                    </div>
+                                    <div class="field">
+                                        <input type="tel" name="number" onchange="document.getElementById('number').value = $(this).val();" required="">
+                                        <label for="number">Mobile Number</label>
+                                    </div>
+                                </form>
+                                <button class="action-pink-form vndr-ct-filtrflw-lgn-web desktop">See Planners</button>
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- Swiper -->
+        </div>
     </section>
+
     <section class="sec">
         <div class="container-fluide">
             <div class="row">
@@ -149,7 +209,9 @@
     </section>
 
 
-
+    <div id="div1" style="width:80px;height:80px;display:none;background-color:red;"></div><br>
+    <div id="div2" style="width:80px;height:80px;display:none;background-color:green;"></div><br>
+    <div id="div3" style="width:80px;height:80px;display:none;background-color:blue;"></div>
 
 
 
@@ -176,24 +238,29 @@
     <!-- script -->
     <script src="<?php echo base_url()?>assets/js/jquery-3.4.1.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/materialize.min.js"></script>
-    <script src="<?php echo base_url()?>assets/js/vue.min.js"></script>
+    <!-- <script src="<?php echo base_url()?>assets/js/vue.min.js"></script> -->
     <script type="text/javascript" src="<?php echo base_url()?>assets/css/slick/slick.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/axios.min.js"></script>
     <script src="<?php echo base_url()?>assets/js/slimselect.min.js"></script>
-    <script src="<?php echo base_url()?>assets/js/script.js"></script>
-    <script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+    <script src="<?php echo base_url()?>assets/js/swiper.js"></script>
 
     <script>
-        var swiper = new Swiper('.swiper-container', {
-            effect: 'flip',
-            grabCursor: true,
-            pagination: {
-                el: '.swiper-pagination',
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
+        $(function() {
+            $("#datep").datepicker();
+        });
+        $('#myform > input').on('input', function() {
+            var empty = false;
+            $('form > input, form > select').each(function() {
+                if ($(this).val() == '') {
+                    empty = true;
+                }
+            });
+
+            if (empty) {
+                $('#register').attr('disabled', 'disabled');
+            } else {
+                $('#register').removeAttr('disabled');
+            }
         });
     </script>
 </body>
