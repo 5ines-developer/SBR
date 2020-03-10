39706c6a44733b65a995a9b79e04518a1efef331
Rishabh5ine <rishabhm@5ine.in>
2020-03-10 12:18:19 +0530

    real-wedding-light-box-error fixing

--- application/views/real/real-wedding-detail.php
+++ application/views/real/real-wedding-detail.php
@@ -22,46 +22,50 @@
     }
     ?>
 
-    <title><?php echo $m_titl ?> | Shaadi Baraati</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="<?php echo $m_descp ?>" />
-    <link rel="canonical" href="<?php echo $m_can ?>" />
-    <meta name="keywords" content="<?php echo $m_key ?>" />
-    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Shaadi Baraati</title>
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/slick/slick.css" />
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/css/slimselect.min.css">
-    <?php $this->load->view('includes/favicon.php');  ?>
-    <style>
-        .img-box{
-            max-height:235px;
-            min-height:235px;
-            width:100%;
-            overflow: hidden;
-        }
-        .wed-detail p{
-            color:black;
-        }
-        .real-weading {
-        padding: 5% 0px;
-        }
-        @media (max-width:991px) and (min-width:768px){
-            .img-box{
-                min-height:144px;
-                width:100%;
+        <title>
+            <?php echo $m_titl ?> | Shaadi Baraati</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="<?php echo $m_descp ?>" />
+        <link rel="canonical" href="<?php echo $m_can ?>" />
+        <meta name="keywords" content="<?php echo $m_key ?>" />
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Shaadi Baraati</title>
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/slick/slick.css" />
+        <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/css/slimselect.min.css">
+        <?php $this->load->view('includes/favicon.php');  ?>
+        <style>
+            .img-box {
+                max-height: 235px;
+                min-height: 235px;
+                width: 100%;
                 overflow: hidden;
             }
-            .card-wed {
-                height: 236px;
+            
+            .wed-detail p {
+                color: black;
             }
-        }
-    </style>
+            
+            .real-weading {
+                padding: 5% 0px;
+            }
+            
+            @media (max-width:991px) and (min-width:768px) {
+                .img-box {
+                    min-height: 144px;
+                    width: 100%;
+                    overflow: hidden;
+                }
+                .card-wed {
+                    height: 236px;
+                }
+            }
+        </style>
 </head>
 
 <body>
@@ -71,10 +75,10 @@
             <div class="container-wedding">
                 <div class="row">
                     <div class="col l12 s12">
-                    <div class="banner-up ">
-                        <h5 class="white-text">Real Wedding</h5>
-                        <p class="tc white-text pad0-11">Customize and send free Online Invitation for your Mehendi, Engagement, Wedding And Reception Events using our wide selection of templates.</p>
-                    </div>
+                        <div class="banner-up ">
+                            <h5 class="white-text">Real Wedding</h5>
+                            <p class="tc white-text pad0-11">Customize and send free Online Invitation for your Mehendi, Engagement, Wedding And Reception Events using our wide selection of templates.</p>
+                        </div>
                         <!-- <div class="">
                             <div class="wedding-name">
                                 <h5>Jashan & Karan</h5>
@@ -83,6 +87,7 @@
                                 
                             </div>
                         </div> -->
+
                     </div>
                 </div>
 
@@ -95,10 +100,19 @@
                         <h5 class="nh-name">Photo Gallery</h5>
                         <div class="bg-depth1 ven-10p" id="gallery">
                             <div class="row">
-                                <div v-for="(src, index) in imgs" :key="index" class="pic pic-over col l3 m4 s12" @click="() => showImg(index)">
+                                <!-- <div v-for="(src, index) in imgs" :key="index" class="pic pic-over col l3 m4 s12" @click="() => showImg(index)">
                                     <img :src="src" class="img-responsive responsive-pp">
                                     <div class="icon-zoom">
                                         <i class="material-icons">zoom_in</i>
+                                    </div>
+                                </div>
+                                <vue-easy-lightbox :visible="visible" :imgs="imgs" :index="index" @hide="handleHide"></vue-easy-lightbox> -->
+                                <div class="gall-gall">
+                                    <div v-for="(src, index) in imgs" :key="index" class="pic pic-over col l3 m4 s12" @click="() => showImg(index)">
+                                        <img :src="src" class="img-responsive responsive-pp">
+                                        <div class="icon-zoom">
+                                            <i class="material-icons">zoom_in</i>
+                                        </div>
                                     </div>
                                 </div>
                                 <vue-easy-lightbox :visible="visible" :imgs="imgs" :index="index" @hide="handleHide"></vue-easy-lightbox>
@@ -108,10 +122,10 @@
         </section>
 
         <form action="" style="display: none">
-            <input id="gal_id" ref="myTestField" type="text" class="validate in-l"name="gal_id"value="<?php echo $this->uri->segment(3); ?>"> 
+            <input id="gal_id" ref="myTestField" type="text" class="validate in-l" name="gal_id" value="<?php echo $this->uri->segment(3); ?>">
         </form>
 
-         <?php if(!empty($related)){ ?>
+        <?php if(!empty($related)){ ?>
         <section class="sec #fafafa grey lighten-5">
             <div class="container-fluide">
                 <div class="row">
@@ -121,26 +135,28 @@
                     <?php
                         foreach($related as $row){
                         ?>
-                         <a href="<?php echo base_url('real-wedding/detail/'.$row->id.'') ?>" >
-                    <div class="col l4 s12 m4">
-                        <div class="card-wed">
-                        <div class="img-box img-real">
-                            <img src="<?php echo base_url().'/'.$row->image;?>" class="img-responsive" alt="">
+                        <a href="<?php echo base_url('real-wedding/detail/'.$row->id.'') ?>">
+                            <div class="col l4 s12 m4">
+                                <div class="card-wed">
+                                    <div class="img-box img-real">
+                                        <img src="<?php echo base_url().'/'.$row->image;?>" class="img-responsive" alt="">
+                                    </div>
+                                    <div class="wed-detail">
+                                        <p><b><?php echo $row->name;?></b> |
+                                            <?php echo $row->city;?>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="wed-detail">
-                                <p><b><?php echo $row->name;?></b> | <?php echo $row->city;?></p>
-                            </div>
-                        </div>
-                    </div>
                         </a>
-                    <?php }?>
+                        <?php }?>
 
-                   
+
                 </div>
-                
+
             </div>
         </section>
-    <?php } ?>
+        <?php } ?>
         </div>
 
         <?php $this->load->view('includes/footer'); ?>
@@ -159,7 +175,7 @@
                     visible: false,
                     index: 0, // default: 0
                     imgs: [],
-                    gal_id:'',
+                    gal_id: '',
                 },
                 methods: {
                     // mobile number check on database
@@ -204,27 +220,25 @@
                     getData: function() {
                         const formData = new FormData();
                         formData.append('gal_id', this.$refs.myTestField.value);
-                        axios.post('<?php echo base_url() ?>real_wedding/gallery',formData)
-                        .then(response => {
-                            if (response.data != '') {
-                                this.imgs = response.data;
-                            }
-                        })
-                        .catch(error => {
-                            console.log(response);
+                        axios.post('<?php echo base_url() ?>real_wedding/gallery', formData)
+                            .then(response => {
+                                if (response.data != '') {
+                                    this.imgs = response.data;
+                                }
+                            })
+                            .catch(error => {
+                                console.log(response);
 
-                        })
+                            })
 
                     },
-                    
+
                 },
                 mounted: function() {
                     this.getData();
                 }
             })
         </script>
-
-
 
 </body>
 
